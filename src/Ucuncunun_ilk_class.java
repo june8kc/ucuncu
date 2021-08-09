d9c4e1b7a60f9c63fe7b60a2244952e63b3d37bf
Cuneyt <june8kc@gmail.com>
2021-08-09 10:12:31 +0300

    first commit

--- src/Ucuncunun_ilk_class.java
+++ src/Ucuncunun_ilk_class.java
@@ -1,5 +1,6 @@
 public class Ucuncunun_ilk_class {
     public static void main(String[] args) {
         System.out.println("ucuncu projenin ilk class i");
+        System.out.println("third");
     }
 }
